def86933d955ce372aaed5f9c33d37a29a4bce4e
xuliangdong <1393679430@qq.com>
2021-09-14 16:48:58 +0800

    feat: 拖动字母滚动列表

--- vue-travel/src/pages/City/components/list.vue
+++ vue-travel/src/pages/City/components/list.vue
@@ -12,44 +12,17 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">上海</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">重庆</div>
+          <div class="button-wrapper" v-for="item in hotCities" :key="item.id">
+            <div class="button">{{ item.name }}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
+        <div class="title border-topbottom">{{ key }}</div>
         <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-      </div>
-
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-      </div>
-
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom" v-for="city in item" :key="city.id">
+            {{ city.name }}
+          </div>
         </div>
       </div>
     </div>
@@ -57,11 +30,24 @@
 </template>
 
 <script>
-import Bscroll from 'better-scroll'
+import Bscroll from "better-scroll";
 export default {
   name: "CityList",
-  mounted () {
-    this.bscroll = new Bscroll(this.$refs.wrapper)
+  props: {
+    cities: Object,
+    hotCities: Array,
+    letter: String,
+  },
+  mounted() {
+    this.bscroll = new Bscroll(this.$refs.wrapper);
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.bscroll.scrollToElement(element)
+      }
+    }
   },
 };
 </script>
--- vue-travel/src/pages/City/index.vue
+++ vue-travel/src/pages/City/index.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header />
     <city-search />
-    <city-list />
-    <city-alphabet />
+    <city-list :cities='cities' :hotCities='hotCities' :letter='letter' />
+    <city-alphabet :cities='cities' @change="handleLetterChange" />
   </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from './components/header.vue'
 import CitySearch from './components/search.vue'
 import CityList from './components/list.vue'
 import CityAlphabet from './components/alphabet.vue'
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
@@ -19,6 +20,30 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  mounted () {
+    this.getCityData()
+  },
+  methods: {
+    getCityData () {
+      axios.get('/api/city.json').then(res => {
+        if (res.data.ret && res.data.data) {
+          const { cities, hotCities} = res.data.data
+          this.cities = cities
+          this.hotCities = hotCities
+        }
+      })
+    },
+    handleLetterChange (value) {
+      this.letter = value
+    }
   }
 }
 </script>
