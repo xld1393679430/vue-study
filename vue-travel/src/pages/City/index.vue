639df4d554c1038213f9904dc144d06057906c0e
xuliangdong <1393679430@qq.com>
2021-09-14 14:53:09 +0800

    feat: city

---
+++ vue-travel/src/pages/City/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <city-header />
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/header.vue'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
